e35a902771d22e295c9b973e20eeb2521d61e6ff
Gana <ganaperkasa77@gmail.com>
2025-10-08 09:59:08 +0700

    Refactor Surat model: define fillable attributes for mass assignment protection

--- app/Models/Surat.php
+++ app/Models/Surat.php
@@ -9,4 +9,18 @@ class Surat extends Model
 {
     protected $table = "adm_surat";
 
+    protected $fillable = [
+        'number',
+        'msk_char',
+        'instansi_id',
+        'address',
+        'perihal',
+        'type',
+        'tanggal',
+        'document',
+        'created_by',
+        'updated_by',
+        'status'
+    ];
+
 }
