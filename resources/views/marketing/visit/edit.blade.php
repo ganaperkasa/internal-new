48e7fbfaa637cb6c6961d491157c3a8dde178dcf
Gana <ganaperkasa77@gmail.com>
2025-10-08 15:33:09 +0700

    Refactor edit view: replace Form facade with HTML form elements for better clarity and maintainability

--- resources/views/marketing/visit/edit.blade.php
+++ resources/views/marketing/visit/edit.blade.php
@@ -13,7 +13,7 @@
               </i>
           </div>
           <div>Ubah Kunjungan
-            
+
           </div>
       </div>
     </div>
@@ -28,51 +28,68 @@
               @endforeach
           </div>
       @endif
-      {!! Form::model($data_edit, [
+      {{-- {!! Form::model($data_edit, [
           'method' => 'PATCH',
           'url' => ['marketing/visit', $data_edit->id],
           'enctype' => 'multipart/form-data'
-      ]) !!}
+      ]) !!} --}}
+       <form method="POST" action="{{ url('marketing/visit/'.$data_edit->id) }}" enctype="multipart/form-data">
+        @csrf
+        @method('PATCH')
 
         <div class="position-relative row form-group">
             <label class="col-sm-3 col-form-label">Instansi</label>
               <div class="col-sm-8">
-                {{ Form::select('instansi_id', $instansi, null, ['class' => 'form-control select2']) }}
+                {{-- {{ Form::select('instansi_id', $instansi, null, ['class' => 'form-control select2']) }} --}}
+                <select name="instansi_id" class="form-control select2">
+                  <option value="">-- Pilih Instansi --</option>
+                  @foreach($instansi as $key => $value)
+                    <option value="{{ $key }}" {{ ( $key == $data_edit->instansi_id) ? 'selected' : '' }}>
+                      {{ $value }}
+                    </option>
+                  @endforeach
+                </select>
               </div>
           </div>
           <h5 class="card-title">Kunjungan</h5>
           <div class="position-relative row form-group">
             <label class="col-sm-3 col-form-label">Tanggal</label>
               <div class="col-sm-2">
-                {!! Form::date('tanggal', null, ['class' => 'form-control'] ) !!}
+                {{-- {!! Form::date('tanggal', null, ['class' => 'form-control'] ) !!} --}}
+                <input type="date" name="tanggal" class="form-control" value="{{ $data_edit->tanggal }}">
               </div>
           </div>
           <div class="position-relative row form-group">
             <label class="col-sm-3 col-form-label">Jam Mulai</label>
               <div class="col-sm-2">
-                {!! Form::text('jam1', null, ['class' => 'form-control datetimepicker3','autocomplete'=>'off'] ) !!}
+                {{-- {!! Form::text('jam1', null, ['class' => 'form-control datetimepicker3','autocomplete'=>'off'] ) !!} --}}
+                <input type="time" name="jam1" class="form-control" value="{{ $data_edit->jam1 }}" autocomplete="off">
               </div>
           </div>
           <div class="position-relative row form-group">
             <label class="col-sm-3 col-form-label">Jam Akhir</label>
               <div class="col-sm-2">
-                {!! Form::text('jam2', null, ['class' => 'form-control datetimepicker3','autocomplete'=>'off'] ) !!}
+                {{-- {!! Form::text('jam2', null, ['class' => 'form-control datetimepicker3','autocomplete'=>'off'] ) !!} --}}
+                <input type="time" name="jam2" class="form-control" value="{{ $data_edit->jam2 }}" autocomplete="off">
               </div>
           </div>
           <div class="position-relative row form-group">
             <label class="col-sm-3 col-form-label">Aktifitas</label>
               <div class="col-sm-8">
-                {!! Form::textarea('keterangan', null, ['class' => 'form-control autosize-input'] ) !!}
+                {{-- {!! Form::textarea('keterangan', null, ['class' => 'form-control autosize-input'] ) !!} --}}
+                <textarea name="keterangan" class="form-control autosize-input">{{ $data_edit->keterangan }}</textarea>
               </div>
           </div>
-         
+
           <div class="position-relative row form-check">
               <div class="col-sm-10 offset-sm-2">
                   <a href="{{ URL::previous() }}" class="btn-shadow mr-3 btn btn-primary">Kembali</a>
-                  {!! Form::button('Simpan', ['class' => 'btn btn-secondary simpan', 'type' => 'submit', 'data-swa-text' => 'Merubah Kunjungan']) !!}
+                  {{-- {!! Form::button('Simpan', ['class' => 'btn btn-secondary simpan', 'type' => 'submit', 'data-swa-text' => 'Merubah Kunjungan']) !!} --}}
+                    <button class="btn btn-secondary simpan" type="submit" data-swa-text="Merubah Kunjungan">Simpan</button>
               </div>
           </div>
-      {!! Form::close() !!}
+      {{-- {!! Form::close() !!} --}}
+      </form>
   </div>
 </div>
 @endsection
